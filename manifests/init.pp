5edddddd51762d10ca3e4d6d0f23a5401b23ef35
jas01 <Through github>
2018-06-13 17:09:43 +0200

    Add posibility to disable service
      In some case we don't want the service to be enable, so add a
    parameter for that.

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,8 +5,9 @@
 class znapzend (
   String $service                    = $::znapzend::params::service,
   Stdlib::Absolutepath $mbuffer_path = $::znapzend::params::mbuffer_path,
-  Optional[Boolean] $use_mbuffer     = true,
   Optional[String] $tsformat         = '%Y-%m-%dT%H:%M:%SZ',
+  Optional[Boolean] $use_mbuffer     = true,
+  Optional[Boolean] $service_enable  = true,
 ) inherits znapzend::params
 {
 
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,17 +1,21 @@
 # @private Manages znapzend service
-class znapzend::service {
+class znapzend::service (
+  Boolean $enabled = $::znapzend::service_enable,
+)
+{
 
-  if $::osfamily == 'FreeBSD' {
-    service { $::znapzend::service :
-      hasrestart => false,
-      enable     => true,
+  if $enabled {
+    if $::osfamily == 'FreeBSD' {
+      service { $::znapzend::service :
+        hasrestart => false,
+        enable     => true,
+      }
+    }
+    else {
+      service { $::znapzend::service :
+        ensure => 'running',
+        enable => true,
+      }
     }
   }
-  else {
-    service { $::znapzend::service :
-      ensure => 'running',
-      enable => true,
-    }
-  }
-
 }
